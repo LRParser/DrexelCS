8f22692ce440f83689ef373ce4fb3357d2283bde
Joseph Heenan <jth82@cs.drexel.edu>
2013-07-01 22:37:24 -0400

    Updated script

--- CS645/Project1/configureVm.sh
+++ CS645/Project1/configureVm.sh
@@ -2,3 +2,36 @@
 
 wget https://www.cs.drexel.edu/~greenie/cs645/etch.sources.list
 cat etch.sources.list >> /etc/apt/sources.list
+# Add required keys for apt
+gpg --keyserver pgpkeys.mit.edu --recv-key 9AA38DCDD55BE302B
+gpg -a --export 9AA38DCDD55BE302B | sudo apt-key add -
+
+# Update apt
+apt-get update
+
+# Install emacs and vim
+apt-get install emacs
+apt-get install vim
+
+# Download the assignment
+
+wget http://www.cs.drexel.edu/~greenie/cs475/projects/cs475-pp1.tar.bz2
+
+tar xjvf cs475-pp1.tar.bz2 
+
+cp -r sploits ~user/
+chown -R user:user ~user/sploits
+cd targets/
+make
+cp * /tmp
+cd /tmp
+chown root:root target? ; chmod 4755 target?; chmod a+r target?.c
+su user
+cd
+ls
+cd sploits/
+ls
+cd /tmp
+ls
+
+chown root:root target? ; chmod 4755 target? ; chmod a+r target?.c
