b5fb6fcab6bd8c3fd27d715442a13c74cc7fcbb7
Joseph Heenan <jth82@cs.drexel.edu>
2013-07-06 14:45:09 -0400

    Samples for StackGuard exploits

---
+++ CS645/Project1/Prob3/vul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+// Example vulnerable program.
+int f (char ** argv)
+{
+        int pipa;   // useless variable
+        char *p;
+        char a[30];
+
+        p=a;
+
+        printf ("p=%x\t -- before 1st strcpy\n",p);
+        strcpy(p,argv[1]);        // <== vulnerable strcpy()
+        printf ("p=%x\t -- after 1st  strcpy\n",p);
+        strncpy(p,argv[2],16);
+        printf("After second strcpy ;)\n");
+}
+
+main (int argc, char ** argv) {
+        f(argv);
+        execl("back_to_vul","",0);  //<-- The exec that fails
+        printf("End of program\n");
+}
+
